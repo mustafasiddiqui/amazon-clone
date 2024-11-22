e692915062485731e62bfaee58d81b94a1d98f32
Mustafa Siddiqui <20732444+mustafasiddiqui@users.noreply.github.com>
2024-11-22 17:15:50 -0600

    Configure React Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React from "react"
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
 
 function App() {
   return (
-    <div className="App">
-      React app
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
