98abe6efc71971ae9d453442cdcf9ab831af8157
Mustafa Siddiqui <20732444+mustafasiddiqui@users.noreply.github.com>
2024-11-27 20:31:27 -0600

    Firebase setup

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+// Use your own Firebase configuration
+export const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
